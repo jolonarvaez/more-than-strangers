12e0c5622cc2edc072d0deb3ec6d8d636cd42bfc
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-06-09 21:04:56 +0800

    added initial loading page skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="text-center flex flex-col lg:flex-row antialiased">
-      <div className="space-y-4">
+    <div className="text-center flex flex-col lg:flex-row antialiased h-screen w-4/5 sm:w-4/5 md:w-screen lg:w-3/5 mx-auto">
+      <div className="space-y-5 flex flex-col lg:h-screen justify-center items-center lg:w-1/2">
         <div className="text-5xl">More Than Strangers</div>
-        <div className="mx-auto w-4/5 text-xl">
+        <div className="mx-auto text-xl px-4">
           <p>
             Discover the essence of connection in "More Than Strangers", an
             engaging social interaction game inspired by "We're Not Strangers."
@@ -17,17 +17,26 @@ export default function Home() {
             you ready to unveil the questions that make us more than strangers?{" "}
           </p>
         </div>
-        <img className="mx-auto" src="figure1.svg" />
+        <img className="mx-auto h-1/2" src="figure1.svg" />
       </div>
-      <div className="space-y-4 my-4 flex flex-col-reverse lg:flex-col">
-        <img className="mx-auto" src="figure2.svg" />
-        <div>
-          <div className="text-4xl">How to Play</div>
-          <div className="mx-auto w-4/5 text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam
-            enim at feugiat dictum. Phasellus ultrices sem sed arcu rutrum, et
-            luctus nulla fringilla. Nullam ac feugiat nulla. Fusce convallis
-            condimentum erat, ut blandit nisl tempus nec.
+
+      <div className="lg:w-1/2 space-y-5 my-4 flex flex-col-reverse lg:flex-col justify-center items-center">
+        <img className="mx-auto mt-8 lg:mt-0" src="figure2.svg" />
+        <div className="space-y-5">
+          <div className="space-y-2">
+            <div className="text-4xl">How to Play</div>
+            <div className="mx-auto px-4 text-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              aliquam enim at feugiat dictum. Phasellus ultrices sem sed arcu
+              rutrum, et luctus nulla fringilla. Nullam ac feugiat nulla. Fusce
+              convallis condimentum erat, ut blandit nisl tempus nec.
+            </div>
+          </div>
+          <div className="space-y-6">
+            <div className="text-3xl">Ready to Play?</div>
+            <button className="px-6 py-2 rounded-lg shadow-lg transition duration-300 ease-in-out border-purple border-2 hover:bg-purple hover:text-white text-purple">
+              <div className="text-xl font-medium">Start</div>
+            </button>
           </div>
         </div>
       </div>
