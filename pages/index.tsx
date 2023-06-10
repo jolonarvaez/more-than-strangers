30cc106ec50343898b268c8040dc42925c4452ca
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-06-11 00:38:56 +0800

    added page load animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,16 @@ export default function Home() {
   return (
     <div className="text-center flex flex-col lg:flex-row antialiased w-4/5 sm:w-4/5 md:w-screen lg:w-3/5 mx-auto">
       <div className="space-y-5 flex flex-col justify-center items-center lg:h-screen lg:w-1/2">
-        <div className="text-5xl">More Than Strangers</div>
-        <div className="mx-auto text-xl px-4">
+        <div
+          className="text-5xl animate-in"
+          style={{ "--index": 1 } as React.CSSProperties}
+        >
+          More Than Strangers
+        </div>
+        <div
+          className="mx-auto text-xl px-4 animate-in"
+          style={{ "--index": 2 } as React.CSSProperties}
+        >
           <p>
             Discover the essence of connection in "More Than Strangers", an
             engaging social interaction game inspired by "We're Not Strangers."
@@ -17,13 +25,24 @@ export default function Home() {
             you ready to unveil the questions that make us more than strangers?{" "}
           </p>
         </div>
-        <img className="mx-auto h-1/2" src="figure1.svg" />
+        <img
+          className="mx-auto h-1/2 animate-in"
+          src="figure1.svg"
+          style={{ "--index": 3 } as React.CSSProperties}
+        />
       </div>
 
       <div className="lg:w-1/2 space-y-5 my-4 flex flex-col-reverse lg:flex-col justify-center items-center">
-        <img className="mx-auto mt-8 lg:mt-0" src="figure2.svg" />
+        <img
+          className="mx-auto mt-8 lg:mt-0 animate-in"
+          src="figure2.svg"
+          style={{ "--index": 1 } as React.CSSProperties}
+        />
         <div className="space-y-5">
-          <div className="space-y-2">
+          <div
+            className="space-y-2 animate-in"
+            style={{ "--index": 2 } as React.CSSProperties}
+          >
             <div className="text-4xl">How to Play</div>
             <div className="mx-auto px-4 text-xl">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
@@ -32,7 +51,10 @@ export default function Home() {
               convallis condimentum erat, ut blandit nisl tempus nec.
             </div>
           </div>
-          <div className="space-y-6">
+          <div
+            className="space-y-6 animate-in"
+            style={{ "--index": 3 } as React.CSSProperties}
+          >
             <div className="text-3xl">Ready to Play?</div>
             <button className="px-6 py-2 rounded-lg shadow-lg transition duration-300 ease-in-out border-purple border-2 hover:bg-purple hover:text-white text-purple">
               <div className="text-xl font-medium">Start</div>
